3ef2f5415a2b748a8b46a274edcc7842265037d3
Samantha Genevay <samanthagenevay@gmail.com>
2023-03-09 08:58:27 -0800

    Made form button sticky, began work on card detail modal

--- src/components/BoardGameCard.js
+++ src/components/BoardGameCard.js
@@ -1,9 +1,10 @@
 import React, {useState} from "react";
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Modal } from 'semantic-ui-react'
 
 function BoardGameCard ({eBO, handleFavoriteUpdate}) {
 
     const [toggleFavorited, setToggleFavorited] = useState(eBO.favorited)
+    const [openCardModal, setOpenCardModal] = useState(false);
     
     const handleFavoriteClick = (eventObj) => {
         const isFavorited = !toggleFavorited;
@@ -11,36 +12,57 @@ function BoardGameCard ({eBO, handleFavoriteUpdate}) {
         handleFavoriteUpdate(eBO.id, isFavorited)
     }
     
+    const handleBoardGameCardClick = () => {
+        setOpenCardModal(true)
+    }
 
     return (
-        <Card className="four wide column">
-            <Image 
-                label={{ as: 'a', corner: 'right', icon: toggleFavorited ? ("heart") : ("heart outline")}}
-                src={eBO.image} 
-                alt = {eBO.name} 
-                wrapped ui={false}
-                onClick = {handleFavoriteClick} 
-            />
-            <Card.Content>
-                <Card.Header>{eBO.name}</Card.Header>
-                <Card.Meta>
-                    <span className='date'>Joined in 2015</span>
-                </Card.Meta>
-                <Card.Description>
-                    {eBO.shortdescription}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <p>
-                    <Icon name='users' />
-                    {eBO.playerCount}
-                </p>
-                <p>
-                    <Icon name='clock'/>
-                    {eBO.playTime}
-                </p>
-            </Card.Content>
-        </Card>
+        <div>
+            <div>
+                <Modal
+                    onClose={() => setOpenCardModal(false)}
+                    open={openCardModal}
+                >
+                    <Modal.Header>Add a Board Game!</Modal.Header>
+                    <Modal.Content>
+                        Testing
+                    </Modal.Content>
+                    <Modal.Actions>
+                        Hello
+                    </Modal.Actions> 
+                </Modal>
+            </div>
+            <div>
+                <Card className="four wide column" onClick={handleBoardGameCardClick}>
+                    <Image 
+                        label={{ as: 'a', corner: 'right', icon: toggleFavorited ? ("heart") : ("heart outline") }}
+                        src={eBO.image} 
+                        alt = {eBO.name} 
+                        wrapped ui={false}
+                        onClick = {handleFavoriteClick} 
+                    />
+                    <Card.Content>
+                        <Card.Header>{eBO.name}</Card.Header>
+                        <Card.Meta>
+                            <span className='date'>Joined in 2015</span>
+                        </Card.Meta>
+                        <Card.Description>
+                            {eBO.shortdescription}
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <p>
+                            <Icon name='users' />
+                            {eBO.playerCount}
+                        </p>
+                        <p>
+                            <Icon name='clock'/>
+                            {eBO.playTime}
+                        </p>
+                    </Card.Content>
+                </Card>
+            </div>
+        </div>
 
 
 
--- src/components/BoardGameContainer.js
+++ src/components/BoardGameContainer.js
@@ -10,37 +10,44 @@ function BoardGameContainer ({boardGameArray, handleFormSubmission, handleFavori
     const [open, setOpen] = React.useState(false)
 
     return(
-        <div className="ui grid container">          
-            <Modal
-                onClose={() => setOpen(false)}
-                onOpen={() => setOpen(true)}
-                open={open}
-                trigger={<Button className = "big ui button modalbutton"><MdPostAdd/></Button>}
-                size= 'small'
-            >
-            <Modal.Header>Add a Board Game!</Modal.Header>
-                <Modal.Content>
-                    <BoardGameForm setOpen = {setOpen}handleFormSubmission={handleFormSubmission} />
-                </Modal.Content>
-                <Modal.Actions>
-                    <Button
-                        form="newBoardGameSubmit" 
-                        type="submit" 
-                    >
-                        Submit
-                    </Button>
-                </Modal.Actions> 
-            </Modal>
-            {boardGameArray.map((eBO) => {
-                return (
-                    <BoardGameCard 
-                        key = {eBO.id} 
-                        eBO = {eBO}
-                        handleFavoriteUpdate = {handleFavoriteUpdate}
-                    />
-                )
-            }
-            )}
+        <div className="modalParent">
+            <h1>Testing</h1>
+            <div className="modalContainer">         
+                <Modal
+                    onClose={() => setOpen(false)}
+                    onOpen={() => setOpen(true)}
+                    open={open}
+                    trigger={<Button className = "big ui button modalbutton"><MdPostAdd size={40}/></Button>}
+                    size= 'small'
+                >
+                <Modal.Header>Add a Board Game!</Modal.Header>
+                    <Modal.Content>
+                        <BoardGameForm setOpen = {setOpen}handleFormSubmission={handleFormSubmission} />
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button
+                            form="newBoardGameSubmit" 
+                            type="submit" 
+                        >
+                            Submit
+                        </Button>
+                    </Modal.Actions> 
+                </Modal>
+            </div>
+            <div>
+                <div className="ui grid container vertically padded">
+                    {boardGameArray.map((eBO) => {
+                        return (
+                            <BoardGameCard 
+                                key = {eBO.id} 
+                                eBO = {eBO}
+                                handleFavoriteUpdate = {handleFavoriteUpdate}
+                            />
+                        )
+                    }
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
