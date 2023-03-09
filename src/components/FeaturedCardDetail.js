bb1ca5e5f62cbdbd2ffbd5cf1eea37ec9d2ba15c
Brynfor Morris <brynfor.morris@gmail.com>
2023-03-09 14:58:45 -0600

    fixed featuredCardDetailBug

--- src/components/FeaturedCardDetail.js
+++ src/components/FeaturedCardDetail.js
@@ -7,26 +7,23 @@ import { Header } from 'semantic-ui-react'
 function FeaturedCardDetail ({showDetails, paragraphBreaker}) {
 
     const {eBGID} = useParams();
-
+    
     const [game, setGame] = useState({})
 
-    
-
     useEffect(()=>{
-        
+
         fetch (`http://localhost:4000/boardgames/${eBGID}`)
             .then(r=>r.json())
             .then(setGame)
 
     },[eBGID])
 
-
-    console.log(showDetails);
-
+    const newVariable = game.longdescription ? paragraphBreaker(game.longdescription)[0] : null
+    
     return(
         <div className="featuredCardInfo">
             <Header as="h3" textAlign="center" className="featuredCardHeader">
-                {showDetails ? <div>{game.name}<br /><h4>{paragraphBreaker(game.longdescription)[0]}</h4></div> : ""}
+                {showDetails ? <div>{game.name}<br /><h4>{newVariable}</h4></div> : ""}
             </Header>
         </div>
     )
